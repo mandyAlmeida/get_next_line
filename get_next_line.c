a80aed26fcba09fef9c21fcc660b63f2d7e2036c
Amanda Ferreira Ramos Almeida <amaferre@c2r11p1.42sp.org.br>
2024-11-20 21:04:24 -0300

    Correção GNL

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amaferre <amaferre@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 20:24:58 by amaferre          #+#    #+#             */
-/*   Updated: 2024/11/10 22:13:54 by amaferre         ###   ########.org.br   */
+/*   Updated: 2024/11/13 00:58:02 by amaferre         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ static char	*ft_read_line(int fd, char *buf, char *save)
 	char	*temp;
 
 	read_line = 1;
-	while (read_line != '\0')
+	while (read_line)
 	{
 		read_line = read(fd, buf, BUFFER_SIZE);
 		if (read_line == -1)
-			return (0);
+			return (NULL);
 		else if (read_line == 0)
 			break ;
 		buf[read_line] = '\0';
@@ -56,7 +56,7 @@ static char	*ft_extract_line(char *line)
 	while (line[count] != '\n' && line[count] != '\0')
 		count++;
 	if (line[count] == '\0' || line[1] == '\0')
-		return (0);
+		return (NULL);
 	save = ft_substr(line, count + 1, ft_strlen(line) - count);
 	if (*save == '\0')
 	{
@@ -74,10 +74,10 @@ char	*get_next_line(int fd)
 	static char	*save;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (0);
+		return (NULL);
 	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
-		return (0);
+		return (NULL);
 	line = ft_read_line(fd, buf, save);
 	free(buf);
 	buf = NULL;
@@ -85,25 +85,4 @@ char	*get_next_line(int fd)
 		return (NULL);
 	save = ft_extract_line(line);
 	return (line);
-}
-
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char **argv)
-{
-    int     fd;
-
-	if (argc < 2)
-		fd = 0;
-	else
-		fd = open(argv[1], O_RDONLY);
-	char *ptr = get_next_line(fd);
-	printf("%s", ptr);
-	free(ptr);
-    close(fd);
-	ptr = get_next_line(-1);
-    return (0);
 }
